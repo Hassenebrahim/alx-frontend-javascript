a2b874cb60983ba730716e25a2d9512ea52dcfd5
Hassenebrahim <99370403+Hassenebrahim@users.noreply.github.com>
2022-12-27 14:31:45 +0300

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,56 +1,38 @@
-/* eslint-disable import/extensions */
-// eslint-disable-next-line no-unused-vars
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
-export default class Pricing {
+class Pricing {
   constructor(amount, currency) {
-    // name (String)
-    if (typeof amount !== 'number') {
-      throw new Error('amount must be a number');
+    if (amount) {
+      this._amount = amount;
     }
-    this._amount = amount;
-
-    // length (Number)
-    if (typeof currency !== 'object') {
-      throw new Error('currency must be a object');
+    if (currency instanceof Currency) {
+      this._currency = currency;
     }
-    this._currency = currency;
   }
 
-  // Implement a getter and setter for each attribute.
   get amount() {
     return this._amount;
-  }
-
-  set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw new Error('amount must be a number');
-    }
-    this._amount = amount;
   }
 
   get currency() {
     return this._currency;
   }
 
+  set amount(amount) {
+    this._amount = amount;
+  }
+
   set currency(currency) {
-    if (typeof currency !== 'object') {
-      throw new Error('currency must be a Object');
-    }
     this._currency = currency;
   }
 
-  displayFullPrice(Currency) {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number') {
-      throw new Error('amount must be a number');
-    }
-    if (typeof conversionRate !== 'number') {
-      throw new Error('conversionRate must be a number');
-    }
     return amount * conversionRate;
   }
 }
+
+export default Pricing;
